47f5e0ea7225e665b05610af12a1d5754f753d1a
Bogdan Bojan <hello@bogdanbojan.com>
2025-12-24 06:57:42 +0200

    Add solutions to quiz2

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,22 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        input
+            .into_iter()
+            .map(|(name, command)| match command {
+                Command::Uppercase => name.to_uppercase(),
+                Command::Trim => name.trim().to_string(),
+                Command::Append(n) => {
+                    let mut s = name;
+                    for _ in 0..n {
+                        s.push_str("bar");
+                    }
+                    s
+                }
+            })
+            .collect()
+    }
 }
 
 fn main() {
@@ -37,8 +52,8 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
     use super::Command;
+    use crate::my_module::transformer;
 
     #[test]
     fn it_works() {
